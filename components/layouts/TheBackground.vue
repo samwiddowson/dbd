9899c625cc1e123e1b129d8eb8c530f71faedebe
Sam Widdowson <samwid@gmail.com>
2024-02-26 21:21:21 +0000

    Correct path to public background image

--- components/layouts/TheBackground.vue
+++ components/layouts/TheBackground.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="fixed left-0 top-0 -z-10 h-full w-full bg-[url('../../public/doom.jpg')] bg-center"
+        class="fixed left-0 top-0 -z-10 h-full w-full bg-[url('../../doom.jpg')] bg-center"
     >
         <div class="left-0 top-0 h-full w-full bg-black/80"></div>
     </div>
