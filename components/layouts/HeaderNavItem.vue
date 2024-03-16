e9ae18a5870b24f67c75c4f894d6f855f4d0cc0b
Sam Widdowson <samwid@gmail.com>
2024-03-16 20:59:14 +0000

    Create separate component for nav item

--- components/layouts/HeaderNavItem.vue
+++ components/layouts/HeaderNavItem.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-import { cn } from "../../utils/cn"
+const NavItem = defineNuxtLink({
+    componentName: "HeaderNavItem",
+    activeClass: "active",
+})
 </script>
 
 <template>
     <li class="block w-full text-right sm:inline-block sm:w-32 sm:text-center">
-        <NuxtLink
+        <NavItem
             :class="
                 cn(
                     `border-b-2 border-black/0 px-2 pt-1 text-base text-black
-                     transition-all duration-500 hover:font-bold
+                     transition-all duration-300 hover:font-bold
                      active:font-bold sm:text-xl`
                 )
             "
             :to="$attrs.to"
-            active-class="border-b-2 border-black"
         >
             <slot></slot>
-        </NuxtLink>
+        </NavItem>
     </li>
 </template>
+
+<style scoped>
+.active {
+    border-color: black;
+}
+</style>
