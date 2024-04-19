58cbc3a228b7a86bfecac00ca6324af3ffbd789f
Sam Widdowson <samwid@gmail.com>
2024-04-19 20:38:57 +0100

    Reconfigure eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 // @ts-ignore
 export default defineNuxtConfig({
     components: [{ path: "~/components/", pathPrefix: false }],
-    modules: ["@nuxtjs/eslint-module", "@nuxtjs/sitemap", "@nuxt/image"],
+    modules: ["@nuxt/eslint", "@nuxtjs/sitemap", "@nuxt/image"],
+    eslint: {},
     // plugins: ["~/plugins/db.server.js"],
     devtools: { enabled: true },
     css: ["~/assets/css/main.css"],
