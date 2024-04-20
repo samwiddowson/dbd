92a401ef36194eef19681f66488a66aee6a62392
Sam Widdowson <samwid@gmail.com>
2024-04-20 19:03:39 +0100

    Add skip to content button

---
+++ components/layouts/SkipToContent.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="absolute w-full -top-24 h-24 grid items-center">
+        <a href='#main-content' class="bg-amber-400 rounded-2xl px-4 py-2 text-black font-semibold">Skip to Main
+            Content</a>
+    </div>
+</template>
