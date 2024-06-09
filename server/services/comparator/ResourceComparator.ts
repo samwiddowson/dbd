89b06c7064e10db81a6f83fb30b73a2417894b0f
LordEntr0py <samwid@gmail.com>
2024-06-09 22:15:34 +0100

    Store texture counts in ResourceComparator

--- server/services/comparator/ResourceComparator.ts
+++ server/services/comparator/ResourceComparator.ts
@@ -9,13 +9,37 @@ export default class ResourceComparator {
     }
 
     addMap(map: ParsedMap) {
-        this.db.prepare("CREATE TABLE @name (texture_name)").run({
-            name: `${map.name}_textures`,
-        })
-        this.db.prepare("CREATE TABLE @name (doomednum)").run({
-            name: `${map.name}_things`,
-        })
+        //cannot bind parameters for table name so need to run our own attempt
+        //  at injection-safe code
+        //essentially, we should throw an error if we see anything non-alphanumeric
+        if (map.name !== map.name.replace(/[^A-Za-z0-9_]/, "")) {
+            throw new Error(
+                `ERROR IN MAP NAME: map has illegal name, ${map.name}`
+            )
+        }
+        const textureTableName = `${map.name}_textures`
+        const doomednumTableName = `${map.name}_doomednums`
 
-        //TODO: insert map data info into table
+        this.db
+            .prepare(`CREATE TABLE ${textureTableName} (texture_name, count)`)
+            .run()
+        this.db
+            .prepare(`CREATE TABLE ${doomednumTableName} (doomednum, count)`)
+            .run()
+
+        for (const textureName of Object.keys(map.textureCounts)) {
+            this.db
+                .prepare(
+                    `
+                            INSERT INTO ${textureTableName}
+                                        (texture_name, count)
+                                 VALUES (@textureName, @count)
+                    `
+                )
+                .run({
+                    textureName: textureName,
+                    count: map.textureCounts[textureName],
+                })
+        }
     }
 }
--- server/services/tests/services/comparator/ResourceComparator.test.ts
+++ server/services/tests/services/comparator/ResourceComparator.test.ts
@@ -2,33 +2,36 @@ import { describe, it, expect, vi } from "vitest"
 import ParsedMap from "~/server/services/model/ParsedMap"
 import ResourceComparator from "~/server/services/comparator/ResourceComparator"
 
-const mocks = vi.hoisted(() => {
-    const run = vi.fn()
-
-    const prepare = vi.fn(() => {
-        return { run }
-    })
-
-    return {
-        db: {
-            default: vi.fn(() => {
-                return { prepare }
-            }),
-        },
-        prepare: prepare,
-        run: run,
-    }
-})
-
-vi.mock("better-sqlite3", () => {
-    return mocks.db
-})
+//MOCKING SQLITE3 SEEMS OVERLY COMPLEX AND ALSO LESS USEFUL THAN ALLOWING
+//  ResourceComparator to create the in-memory database and then querying it in tests
+// const mocks = vi.hoisted(() => {
+//     const run = vi.fn()
+//
+//     const prepare = vi.fn(() => {
+//         return { run }
+//     })
+//
+//     return {
+//         db: {
+//             default: vi.fn(() => {
+//                 return { prepare }
+//             }),
+//         },
+//         prepare: prepare,
+//         run: run,
+//     }
+// })
+//
+// vi.mock("better-sqlite3", () => {
+//     return mocks.db
+// })
 
 describe("constructor", () => {
-    it("makes expected sqllite3 call", () => {
-        new ResourceComparator()
-        expect(mocks.db.default).toHaveBeenCalledOnce()
-        expect(mocks.db.default.mock.calls[0]).toEqual([":memory:"])
+    it("creates in-memory sqllite3 databse when called", () => {
+        const r = new ResourceComparator()
+        expect(r.db.memory).toBe(true)
+        // expect(mocks.db.default).toHaveBeenCalledOnce()
+        // expect(mocks.db.default.mock.calls[0]).toEqual([":memory:"])
     })
 })
 
@@ -36,25 +39,41 @@ describe("addMap", () => {
     it("creates expected tables", () => {
         const TEST_MAP_NAME = "TESTMAP_01"
         const r = new ResourceComparator()
-        const testMap = new ParsedMap(TEST_MAP_NAME)
+        const testMap = new ParsedMap(TEST_MAP_NAME, "Lump")
+        testMap.textureCounts = { TEX1: 10, TEX2: 20, TEX3: 30 }
 
         r.addMap(testMap)
 
-        expect(mocks.prepare).toHaveBeenCalledTimes(2)
+        for (const textureName of Object.keys(testMap.textureCounts)) {
+            const dbRow = r.db
+                .prepare(
+                    `
+                     SELECT *
+                       FROM TESTMAP_01_textures
+                      WHERE texture_name = @textureName`
+                )
+                .get({
+                    textureName: textureName,
+                }) as { texture_name: string; count: number }
+            expect(dbRow.texture_name).toBe(textureName)
+            expect(dbRow.count).toBe(testMap.textureCounts[textureName])
+        }
 
-        expect(mocks.prepare.mock.calls[0].pop()).toBe(
-            "CREATE TABLE @name (texture_name)"
-        )
-        expect(mocks.prepare.mock.calls[1].pop()).toBe(
-            "CREATE TABLE @name (doomednum)"
-        )
-
-        expect(mocks.run).toHaveBeenCalledTimes(2)
-        expect(mocks.run.mock.calls[0].pop()).toEqual({
-            name: `${TEST_MAP_NAME}_textures`,
-        })
-        expect(mocks.run.mock.calls[1].pop()).toEqual({
-            name: `${TEST_MAP_NAME}_things`,
-        })
+        // expect(mocks.prepare).toHaveBeenCalledTimes(2)
+        //
+        // expect(mocks.prepare.mock.calls[0].pop()).toBe(
+        //     "CREATE TABLE @name (texture_name)"
+        // )
+        // expect(mocks.prepare.mock.calls[1].pop()).toBe(
+        //     "CREATE TABLE @name (doomednum)"
+        // )
+        //
+        // expect(mocks.run).toHaveBeenCalledTimes(2)
+        // expect(mocks.run.mock.calls[0].pop()).toEqual({
+        //     name: `${TEST_MAP_NAME}_textures`,
+        // })
+        // expect(mocks.run.mock.calls[1].pop()).toEqual({
+        //     name: `${TEST_MAP_NAME}_things`,
+        // })
     })
 })
