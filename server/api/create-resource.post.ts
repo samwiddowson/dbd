1e4167d52d6e1b5836068e30558b61ca95241dc6
LordEntr0py <samwid@gmail.com>
2024-06-22 23:30:07 +0100

    A little cleanup of some existing code

--- server/api/create-resource.post.ts
+++ server/api/create-resource.post.ts
@@ -1,27 +1,24 @@
-import {
-    ResourceData,
-    saveResourceData as save,
-} from "~/server/services/resource-service"
+import type { ResourceData } from "~/server/services/resource-service"
+import { saveResourceData as save } from "~/server/services/resource-service"
 
 export default defineEventHandler(async (event) => {
-    const formData: any = await readMultipartFormData(event)
+    const formData = await readMultipartFormData(event)
 
     if (!formData) return
 
-    console.log(formData)
-
     function extract(fieldName: string) {
-        return formData!.find((item: any) => item.name === fieldName)
+        return formData?.find((item) => item.name === fieldName)
     }
 
-    const resourceData: ResourceData = {
-        name: extract("title")?.data?.toString("utf8")!,
-        description: extract("description")?.data?.toString("utf8")!,
-        imageFile: extract("image"),
+    const resourceData = {
+        name: extract("title")?.data?.toString("utf8") ?? "",
+        description: extract("description")?.data?.toString("utf8") ?? "",
+        imageFile: Buffer.from(extract("image")?.data ?? "") as image,
         slug: "",
     }
 
     console.log("resourceData", resourceData)
+    //TODO: check data and throw/log error and respond with error if it's broken
 
     save(resourceData)
 })
--- server/services/resource-service.ts
+++ server/services/resource-service.ts
@@ -9,32 +9,32 @@ export interface ResourceData {
     name: string
     slug: string
     description: string
-    imageFile: any
-    dataFile?: any //TODO: remove optional modifier once implemented
+    imageFile: Buffer
+    dataFile?: Buffer //TODO: remove optional modifier once implemented
     creatorId: string
 }
 
 export async function saveResourceData({
     name,
     description,
-    imageFile: image,
-    data,
+    imageFile,
+    dataFile,
 }: ResourceData) {
     const resourceSlug = generateSlug(name)
 
     //TODO - ensure slug is unique
+    const imageFileName = await imageRepo.saveFile(imageFile, resourceSlug)
+    const dataFileName = await fileRepo.saveFile(dataFile, resourceSlug)
 
-    const resourceInfo: ResourceData = {
+    const resourceInfo = {
         name,
         slug: resourceSlug,
         description,
-        imageFile: "",
-        dataFile: "",
+        imageFile: imageFileName,
+        dataFile: dataFileName,
         creatorId: "",
     }
 
-    resourceInfo.imageFile = await imageRepo.saveFile(image, resourceSlug)
-    resourceInfo.dataFile = await fileRepo.saveFile(data, resourceSlug)
     saveResourceInfo(resourceInfo)
 }
 
