520817d7ccd28b2c9a1de0907a653b0dca41c527
Sam Widdowson <samwid@gmail.com>
2024-06-24 20:19:21 +0100

    Rewrite MapParser with private members and generator function

--- server/services/parsers/wad/WadParser.ts
+++ server/services/parsers/wad/WadParser.ts
@@ -15,39 +15,44 @@ interface DirectoryEntry {
     name: string
 }
 
-function readDirectory(rawData: Buffer, dirOffset: number, numlumps: number) {
-    const d: DirectoryEntry[] = []
-    for (let i = 0; i < numlumps; i++) {
-        const filePosOffset = dirOffset + i * 16
-        const sizeOffset = filePosOffset + 4
-        const nameOffset = sizeOffset + 4
-        const entry: DirectoryEntry = {
-            filepos: rawData.readIntLE(filePosOffset, 4),
-            size: rawData.readIntLE(sizeOffset, 4),
-            name: readWadLumpText(rawData, nameOffset),
-        }
-        d.push(entry)
-    }
-    return d
-}
-
 export default class WadParser {
     rawData: Buffer
     header: Header | undefined
-    directory: DirectoryEntry[]
+    directory: DirectoryEntry[] = []
 
-    constructor(rawData: Buffer) {
-        this.rawData = rawData
-        this.header = {
+    #readHeader() {
+        return {
             identification: this.rawData.toString("ascii", 0, 4),
             numlumps: this.rawData.readIntLE(4, 4),
             infotableofs: this.rawData.readIntLE(8, 4),
         }
-        this.directory = readDirectory(
-            this.rawData,
-            this.header.infotableofs,
-            this.header.numlumps
-        )
+    }
+
+    *#directoryEntryReader() {
+        const entryOffset = this.header!.infotableofs
+        for (let i = 0; i < this.header!.numlumps; i++) {
+            const filePosOffset = entryOffset + i * 16
+            const sizeOffset = filePosOffset + 4
+            const nameOffset = sizeOffset + 4
+
+            const entry: DirectoryEntry = {
+                filepos: this.rawData.readIntLE(filePosOffset, 4),
+                size: this.rawData.readIntLE(sizeOffset, 4),
+                name: readWadLumpText(this.rawData, nameOffset),
+            }
+            yield entry
+        }
+    }
+
+    constructor(rawData: Buffer) {
+        this.rawData = rawData
+        this.header = this.#readHeader()
+        const directoryEntries = this.#directoryEntryReader()
+        let entry = directoryEntries.next()
+        while (!entry.done) {
+            this.directory.push(entry.value)
+            entry = directoryEntries.next()
+        }
     }
 
     getMapData() {
