7eb3495934f3fcf60aca8927b70448d6e574ff48
Sam Widdowson <samwid@gmail.com>
2025-05-13 15:21:32 +0100

    Upgrade Nuxt and other libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,35 @@
+import tailwindcss from "@tailwindcss/vite"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// @ts-expect-error linter cannot find defineNuxtConfig
 export default defineNuxtConfig({
     components: [{ path: "~/components/", pathPrefix: false }],
+
     modules: [
         "@nuxt/eslint",
         "@nuxtjs/sitemap",
         "@nuxt/image",
         "@nuxt/test-utils/module",
     ],
-    eslint: {},
+
     // plugins: ["~/plugins/db.server.js"],
     devtools: { enabled: true },
+
     css: ["~/assets/css/main.css"],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
+
+    vite: {
+        plugins: [tailwindcss()],
     },
+
     app: {
         head: {
             htmlAttrs: { lang: "en" },
             title: "DBD | Double-Barrel Delivery",
         },
     },
+
     runtimeConfig: {
         loggingLevel: "TRACE",
     },
-    site: {
-        url: "http://localhost:10667",
-    },
+
+    compatibilityDate: "2025-05-13",
 })
